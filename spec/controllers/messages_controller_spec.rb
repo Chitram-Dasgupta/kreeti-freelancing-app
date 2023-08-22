9dca013e99a85a2524fcb033ca0aad087da8b4b8
chitramdasgupta <chitram.dasgupta@kreeti.com>
2023-08-22 16:17:12 +0530

    Fixed the rubocop warnings for the messages controller spec

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -12,31 +12,26 @@ RSpec.describe MessagesController do
     context 'with valid parameters' do
       let(:valid_params) { { room_id: room.id, message: { content: 'Hello' } } }
 
-      it 'creates a new message and broadcasts it' do
+      it 'creates a new message' do
         expect { post :create, params: valid_params, format: :json }.to change(Message, :count).by(1)
+      end
 
-        expect(response).to have_http_status(:ok)
-        expect(response.content_type).to eq 'application/json; charset=utf-8'
-
-        parsed_response = response.parsed_body
-        expect(parsed_response['status']).to eq 'ok'
+      it 'broadcasts the created message' do
+        post :create, params: valid_params, format: :json
+        expect(response.parsed_body['status']).to eq 'ok'
       end
     end
 
     context 'with invalid parameters' do
       let(:invalid_params) { { room_id: room.id, message: { content: '' } } }
 
-      it 'does not create a new message and returns error' do
-        expect do
-          post :create, params: invalid_params, format: :json
-        end.not_to change(Message, :count)
+      it 'does not create a new message' do
+        expect { post :create, params: invalid_params, format: :json }.not_to change(Message, :count)
+      end
 
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to eq 'application/json; charset=utf-8'
-
-        parsed_response = response.parsed_body
-        expect(parsed_response['status']).to eq 'error'
-        expect(parsed_response['errors']).to be_present
+      it 'returns error response' do
+        post :create, params: invalid_params, format: :json
+        expect(response.parsed_body['status']).to eq 'error'
       end
     end
   end
