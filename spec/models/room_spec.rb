23cc4105d022de56183e590f98602809cfa6c98b
chitramdasgupta <chitram.dasgupta@kreeti.com>
2023-08-22 15:53:42 +0530

    Fixed the rubocop warnings for the room model spec

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -4,8 +4,11 @@ require 'rails_helper'
 
 RSpec.describe Room do
   describe 'associations' do
+    let(:room) { create(:room) }
+    let(:user) { create(:user) }
+    let(:other_user) { create(:user) }
+
     it 'has many messages' do
-      room = create(:room)
       message1 = create(:message, room:)
       message2 = create(:message, room:)
 
@@ -13,24 +16,18 @@ RSpec.describe Room do
     end
 
     it 'has many user_rooms' do
-      room = create(:room)
-      user1 = create(:user)
-      user2 = create(:user, :freelancer)
-      user_room1 = create(:user_room, room:, user1:, user2:)
-      user_room2 = create(:user_room, room:, user1:, user2:)
+      user_room1 = create(:user_room, room:, user1: user, user2: other_user)
+      user_room2 = create(:user_room, room:, user1: user, user2: other_user)
 
       expect(room.user_rooms).to include(user_room1, user_room2)
     end
 
     it 'has many users through user_rooms' do
-      room = create(:room)
-      user1 = create(:user)
-      user2 = create(:user)
-      create(:user_room, room:, user1:, user2:)
-      create(:user_room, room:, user1:, user2:)
+      create(:user_room, room:, user1: user, user2: other_user)
+      create(:user_room, room:, user1: user, user2: other_user)
 
       users = room.user_rooms.map { |ur| [ur.user1, ur.user2] }.flatten
-      expect(users).to include(user1, user2)
+      expect(users).to include(user, other_user)
     end
   end
 
@@ -38,14 +35,14 @@ RSpec.describe Room do
     let(:room) { create(:room) }
     let(:first_user) { create(:user) }
     let(:second_user) { create(:user, :freelancer) }
-    let!(:message) { create(:message, room:) }
-    let!(:user_room) { create(:user_room, room:, user1: first_user, user2: second_user) }
 
     it 'destroys associated messages when destroyed' do
+      create(:message, room:)
       expect { room.destroy }.to change(Message, :count).by(-1)
     end
 
     it 'destroys associated user_rooms when destroyed' do
+      create(:user_room, room:, user1: first_user, user2: second_user)
       expect { room.destroy }.to change(UserRoom, :count).by(-1)
     end
   end
