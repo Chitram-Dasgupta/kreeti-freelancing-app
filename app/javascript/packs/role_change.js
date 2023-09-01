8a963096165b8aedea78456c46d053a140919283
chitramdasgupta <chitram.dasgupta@kreeti.com>
2023-09-01 13:17:52 +0530

    Fixed the minor issue regarding the rendering of freelancer fields

--- app/javascript/packs/role_change.js
+++ app/javascript/packs/role_change.js
@@ -2,11 +2,15 @@ window.addEventListener("turbolinks:load", function () {
   var user_role = document.querySelector("#user_role");
 
   if (user_role) {
-    user_role.addEventListener("change", function () {
+    var toggleFreelancerFields = function () {
       var freelancer_fields = document.querySelector("#freelancer_fields");
       if (freelancer_fields) {
-        freelancer_fields.style.display = this.value === "freelancer" ? "block" : "none";
+        freelancer_fields.style.display = user_role.value === "freelancer" ? "block" : "none";
       }
-    });
+    };
+
+    toggleFreelancerFields();
+
+    user_role.addEventListener("change", toggleFreelancerFields);
   }
 });
